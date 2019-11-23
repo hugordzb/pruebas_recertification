c3a8072fa228b6aeccec2adb7177e93a501b4417
Hugo Daniel Rodriguez Bautista <hdrodriguezb@truper.com>
2019-11-23 09:28:34 -0600

    Inicio de cambios

--- src/Configs.js
+++ src/Configs.js
@@ -1,27 +1,17 @@
 const DEV_VARS = {
-  AUDITABLE_SYSTEMS: {
-    TEL: "TEL",
-    CIAT: "CIAT",
-    SAP: "SAP",
-  },
-  SERVERS: {
-    RECERTIFICATION: `http://172.18.8.210:8080/recertification/api/v1`,
-    SSO: `http://localhost:8070/wsso/api/v1`,
-  }
-
-};
-
-const PROD_VARS = {
-  AUDITABLE_SYSTEMS: {
-    TEL: "TEL",
-    CIAT: "CIAT",
-    SAP: "SAP",
-  },
   SERVERS: {
     RECERTIFICATION: `http://172.18.8.210:8080/recertification/api/v1`,
     SSO: `http://172.20.3.28:8070/wsso/api/v1`,
   }
 
+};
+
+const PROD_VARS = {
+  SERVERS: {
+    RECERTIFICATION: `http://172.18.8.210:8080/recertification/api/v1`,
+    SSO: `http://172.20.3.28:8070/wsso/api/v1`,
+  }
+  
 };
 
 export default class Configs {
